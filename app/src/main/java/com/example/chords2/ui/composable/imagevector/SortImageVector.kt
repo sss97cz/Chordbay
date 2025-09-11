b1b9bd21939e4048bcd524a79d9030453de86007
simon <pp99pp88pp77@gmail.com>
2025-09-11 13:12:51 +0200

    experimenting with main screen layout

---
+++ app/src/main/java/com/example/chords2/ui/composable/imagevector/SortImageVector.kt
@@ -0,0 +1,53 @@
+package com.example.chords2.ui.composable.imagevector
+
+
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+val Sort: ImageVector
+    get() {
+        if (_Sort != null) return _Sort!!
+
+        _Sort = ImageVector.Builder(
+            name = "FilterLeft",
+            defaultWidth = 16.dp,
+            defaultHeight = 16.dp,
+            viewportWidth = 16f,
+            viewportHeight = 16f
+        ).apply {
+            path(
+                fill = SolidColor(Color.Black)
+            ) {
+                moveTo(2f, 10.5f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0.5f, -0.5f)
+                horizontalLineToRelative(3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0f, 1f)
+                horizontalLineToRelative(-3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, -0.5f, -0.5f)
+                moveToRelative(0f, -3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0.5f, -0.5f)
+                horizontalLineToRelative(7f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0f, 1f)
+                horizontalLineToRelative(-7f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, -0.5f, -0.5f)
+                moveToRelative(0f, -3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0.5f, -0.5f)
+                horizontalLineToRelative(11f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0f, 1f)
+                horizontalLineToRelative(-11f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, -0.5f, -0.5f)
+            }
+        }.build()
+
+        return _Sort!!
+    }
+
+
+private var _Sort: ImageVector? = null
+
+
+
--- app/src/main/java/com/example/chords2/ui/composable/screen/HomeScreen.kt
+++ app/src/main/java/com/example/chords2/ui/composable/screen/HomeScreen.kt
@@ -2,46 +2,71 @@ package com.example.chords2.ui.composable.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.chords2.data.model.Song // Your domain data class
 import com.example.chords2.ui.composable.component.SongItem
+import com.example.chords2.ui.composable.imagevector.Sort
 import com.example.chords2.ui.composable.navigation.Paths
 import com.example.chords2.ui.composable.topappbar.MyTopAppBar
-import com.example.chords2.ui.composable.topappbar.TopAppBarConfig
 import com.example.chords2.ui.viewmodel.SongViewModel
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
@@ -52,117 +77,254 @@ fun HomeScreen(
     val songs = songViewModel.songs.collectAsState()
     val scope = rememberCoroutineScope()
     var enableEditing by remember { mutableStateOf(false) }
-    Scaffold(
-        topBar = {
-            MyTopAppBar(
-                title = "My Songs",
-                // No navigation icon if it's the start destination without a drawer
-                // Or, if you have a drawer:
-                // navigationIcon = Icons.Filled.Menu,
-                // navigationIconContentDescription = "Open Drawer",
-                // onNavigationIconClick = { /* TODO: Open Drawer */ },
-                actions = {
-                    IconButton(onClick = {
-                        scope.launch {
-                            val newSongId = songViewModel.addNewSongAndGetId(
-                                Song(
-                                    title = "New Song",
-                                    content = ""
-                                )
-                            )
-                            navController.navigate(Paths.EditSongPath.createRoute(newSongId.toString()))
-                        }
-                    }) {
-                        Icon(Icons.Filled.Add, contentDescription = "Add Song")
-                    }
-                    IconButton(onClick = { /* TODO: Search */ }) {
-                        Icon(Icons.Filled.Search, contentDescription = "Search Songs")
-                    }
-                }
-            )
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = modifier.padding(innerPadding).fillMaxSize()
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-//            Button(
-//                onClick = {
-//                    scope.launch {
-//
-//                        val newSongDetails = Song()
-//                        val newSongId = songViewModel.addNewSongAndGetId(newSongDetails)
-//                        navController.navigate(
-//                            Paths.EditSongPath.createRoute(
-//                                songId = newSongId.toString()
-//                            )
-//                        )
-//                    }
-//                }
-//            ) {
-//                Text(text = "Add Song")
-//            }
-//            Button(
-//                onClick = {
-//                    scope.launch {
-//                        songViewModel.deleteAll()
-//                    }
-//                }
-//            ) {
-//                Text(text = "Delete All Songs")
-//            }
-                Button(
-                    onClick = {
-                        enableEditing = !enableEditing
-                    },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = if (enableEditing) {
-                            Color.Green
-                        } else {
-                            Color.Red
-                        }
-                    )
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
+    var selectedTab by remember { mutableIntStateOf(0) }
+    var showOptionsMenu by remember { mutableStateOf(false) }
+    var showFabMenu by remember { mutableStateOf(false) }
+
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .verticalScroll(rememberScrollState())
                 ) {
-                    Image(
-                        imageVector = Icons.Default.Edit,
-                        contentDescription = "Edit Song"
+                    Spacer(Modifier.height(12.dp))
+                    Text(
+                        "Drawer Title",
+                        modifier = Modifier.padding(16.dp),
+                        style = MaterialTheme.typography.titleLarge
                     )
+                    HorizontalDivider()
+
+                    Text(
+                        "Section 1",
+                        modifier = Modifier.padding(16.dp),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    NavigationDrawerItem(
+                        label = { Text("Item 1") },
+                        selected = false,
+                        onClick = { /* Handle click */ }
+                    )
+                    NavigationDrawerItem(
+                        label = { Text("Item 2") },
+                        selected = false,
+                        onClick = { /* Handle click */ }
+                    )
+
+                    HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
+
+                    Text(
+                        "Section 2",
+                        modifier = Modifier.padding(16.dp),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    NavigationDrawerItem(
+                        label = { Text("Settings") },
+                        selected = false,
+                        icon = { Icon(Icons.Outlined.Settings, contentDescription = null) },
+                        badge = { Text("hahaah") }, // Placeholder
+                        onClick = { /* Handle click */ }
+                    )
+                    NavigationDrawerItem(
+                        label = { Text("Help and feedback") },
+                        selected = false,
+                        icon = { Icon(Icons.Outlined.Info, contentDescription = null) },
+                        onClick = { /* Handle click */ },
+                    )
+                    Spacer(Modifier.height(12.dp))
                 }
             }
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                items(songs.value) { songEntity ->
-                    SongItem(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(100.dp),
-                        songTitle = songEntity.title,
-                        songArtist = songEntity.artist,
-                        onSongClick = {
-                            if (!enableEditing) {
-                                navController.navigate(
-                                    Paths.SongPath.createRoute(
-                                        songEntity.id.toString()
-                                    )
-                                )
+        },
+        drawerState = drawerState
+    ) {
+        Scaffold(
+            topBar = {
+                MyTopAppBar(
+                    title = "My Songs",
+                    // No navigation icon if it's the start destination without a drawer
+                    // Or, if you have a drawer:
+                    navigationIcon = Icons.Filled.Menu,
+                    onNavigationIconClick = {
+                        scope.launch {
+                            if (drawerState.isClosed) {
+                                drawerState.open()
                             } else {
-                                navController.navigate(
-                                    Paths.EditSongPath.createRoute(
-                                        songId = songEntity.id.toString()
-                                    )
-                                )
-                            }
-                        },
-                        onDeleteClick = {
-                            scope.launch {
-                                songViewModel.deleteSong(songEntity)
+                                drawerState.close()
                             }
                         }
+                    },
+                    actions = {
+                        IconButton(onClick = { /* TODO: Search */ }) {
+                            Icon(Icons.Filled.Search, contentDescription = "Search Songs")
+                        }
+                        IconButton(onClick = {
+                            scope.launch {
+                                val newSongId = songViewModel.addNewSongAndGetId()
+                                navController.navigate(Paths.EditSongPath.createRoute(newSongId.toString()))
+                            }
+                        }) {
+                            Icon(Icons.Filled.Add, contentDescription = "Add Song")
+                        }
+                        Box {
+                            IconButton(onClick = { showOptionsMenu = !showOptionsMenu }) {
+                                Icon(Icons.Default.MoreVert, contentDescription = "More options")
+                            }
+                            DropdownMenu(
+                                expanded = showOptionsMenu,
+                                onDismissRequest = { showOptionsMenu = false }
+                            ) {
+                                DropdownMenuItem(
+                                    text = { Text("Option 1") },
+                                    onClick = { /* Do something... */ }
+                                )
+                                DropdownMenuItem(
+                                    text = { Text("Option 2") },
+                                    onClick = { /* Do something... */ }
+                                )
+                            }
+                        }
+                    },
+                )
+            },
+            floatingActionButton = {
+                Box {
+                    FloatingActionButton(
+                        shape = CircleShape,
+                        onClick = {
+                            showFabMenu = !showFabMenu
+                        },
+                        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                        contentColor = MaterialTheme.colorScheme.secondary
+                    ) {
+                        Icon(
+                            imageVector = Sort,
+                            contentDescription = "Small floating action button.",
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                    DropdownMenu(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.Center),
+                        expanded = showFabMenu,
+                        onDismissRequest = { showFabMenu = false }
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally),
+                            text = "Sort by:"
+                        )
+                        DropdownMenuItem(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = {
+                                Text(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    text = "Option 2",
+                                    textAlign = TextAlign.Center
+                                )
+                            },
+                            onClick = { showFabMenu = false }
+                        )
+                        DropdownMenuItem(
+                            text = {
+                                Text(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    text = "Option 2",
+                                    textAlign = TextAlign.Center,
+                                )
+                            },
+                            onClick = { showFabMenu = false }
+                        )
+                    }
+                }
+            }
+        ) { innerPadding ->
+            Column(
+                modifier = modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+            ) {
+                PrimaryTabRow(
+                    modifier = Modifier.fillMaxWidth(),
+                    selectedTabIndex = selectedTab,
+                ) {
+                    Tab(
+                        onClick = {
+                            selectedTab = 0
+                        },
+                        selected = selectedTab == 0,
+                        text = {
+                            Text("Tab 1")
+                        }
                     )
+                    Tab(
+                        onClick = {
+                            selectedTab = 1
+                        },
+                        selected = selectedTab == 1,
+                        text = {
+                            Text("Tab 2")
+                        }
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Button(
+                        onClick = {
+                            enableEditing = !enableEditing
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = if (enableEditing) {
+                                Color.Green
+                            } else {
+                                Color.Red
+                            }
+                        )
+                    ) {
+                        Image(
+                            imageVector = Icons.Default.Edit,
+                            contentDescription = "Edit Song"
+                        )
+                    }
+                }
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    items(songs.value) { songEntity ->
+                        SongItem(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(100.dp),
+                            songTitle = songEntity.title,
+                            songArtist = songEntity.artist,
+                            onSongClick = {
+                                if (!enableEditing) {
+                                    navController.navigate(
+                                        Paths.SongPath.createRoute(
+                                            songEntity.id.toString()
+                                        )
+                                    )
+                                } else {
+                                    navController.navigate(
+                                        Paths.EditSongPath.createRoute(
+                                            songId = songEntity.id.toString()
+                                        )
+                                    )
+                                }
+                            },
+                            onDeleteClick = {
+                                scope.launch {
+                                    songViewModel.deleteSong(songEntity)
+                                }
+                            }
+                        )
+                    }
                 }
             }
         }
