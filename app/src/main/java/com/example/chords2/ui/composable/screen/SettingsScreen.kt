09b8045d3e670a1b0c9ad67847198d90b1414e15
simon <pp99pp88pp77@gmail.com>
2025-09-15 13:16:09 +0200

    further changes to SettingsScreen.kt

--- app/src/main/java/com/example/chords2/ui/composable/screen/SettingsScreen.kt
+++ app/src/main/java/com/example/chords2/ui/composable/screen/SettingsScreen.kt
@@ -1,26 +1,42 @@
 package com.example.chords2.ui.composable.screen
 
+import android.inputmethodservice.Keyboard
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.chords2.data.model.util.Settings
+import com.example.chords2.data.model.util.SortBy
 import com.example.chords2.ui.composable.topappbar.MyTopAppBar
 import com.example.chords2.ui.viewmodel.SongViewModel
 
@@ -30,6 +46,9 @@ fun SettingsScreen(
     navController: NavController
 ) {
     val canNavigateBack = navController.previousBackStackEntry != null
+    var isSortMenuExpanded by remember { mutableStateOf(false) }
+    var defoultSortOption by remember { mutableStateOf(SortBy.SONG_NAME) }
+    var isDarkTheme by remember { mutableStateOf(false) }
     Scaffold(
         topBar = {
             MyTopAppBar(
@@ -46,6 +65,7 @@ fun SettingsScreen(
                 .padding(innerPadding)
                 .padding(8.dp),
             horizontalAlignment = Alignment.CenterHorizontally
+
         ) {
             for (setting in Settings.all) {
                 when (setting) {
@@ -54,19 +74,42 @@ fun SettingsScreen(
                             Modifier.fillMaxWidth(),
                             settingName = setting.title,
                         ){
-                            Button({}) {
-                                Text("theme change")
+                            Box(Modifier.clip(CircleShape).background(MaterialTheme.colorScheme.secondaryContainer)){
+                                Row(
+                                    Modifier,
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    verticalAlignment = Alignment.CenterVertically) {
+                                    Text(text = defoultSortOption.displayName, Modifier.padding(start = 2.dp))
+                                    IconButton(onClick = { isSortMenuExpanded = true }) {
+                                        Icon(Icons.Default.ArrowDropDown, contentDescription = null)
+                                    }
+                                    DropdownMenu(
+                                        expanded = isSortMenuExpanded,
+                                        onDismissRequest = { isSortMenuExpanded = false }
+                                    ) {
+                                        for (sortOption in SortBy.entries) {
+                                            DropdownMenuItem(
+                                                text = { Text(sortOption.displayName) },
+                                                onClick = {
+                                                    defoultSortOption = sortOption
+                                                }
+                                            )
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
                     Settings.ThemeSetting -> {
                         SettingsRow(
-                            Modifier.fillMaxWidth(),
                             settingName = setting.title,
                         ){
-                            Button({}) {
-                                Text("theme change")
-                            }
+                            Switch(
+                                checked = isDarkTheme,
+                                onCheckedChange = {
+                                    isDarkTheme = it
+                                }
+                            )
                         }
                     }
                 }
@@ -75,17 +118,16 @@ fun SettingsScreen(
     }
 }
 
-
 @Composable
 private fun SettingsRow(
     modifier: Modifier = Modifier,
     settingName: String,
     content: @Composable () -> Unit
 ) {
-    Column(
-        modifier = modifier.fillMaxWidth(),
-    ) {
 
+    Column(
+        Modifier.fillMaxWidth().padding(start = 8.dp, end = 8.dp),
+    ) {
         Row(
             Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
@@ -93,9 +135,10 @@ private fun SettingsRow(
             Text(text = settingName)
             Spacer(modifier = Modifier.weight(1f))
             content()
+            Spacer(Modifier.padding(bottom = 2.dp))
         }
-        HorizontalDivider()
     }
+    HorizontalDivider()
 }
 
 
