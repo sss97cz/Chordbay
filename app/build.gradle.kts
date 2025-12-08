088d7c0edc550a57705fe8e58bbea45aeb11ab75
simon <pp99pp88pp77@gmail.com>
2025-12-08 23:30:56 +0100

    version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.chordbay.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
